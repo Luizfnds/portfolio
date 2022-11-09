215fb8eeb3bfb926597b2bba91418aa3a1c0dae0
Luiz Eduardo <luizeduardofnds@gmail.com>
2022-11-08 22:13:58 -0300

    Refatoração - 4

--- js/menu-responsivo.js
+++ js/menu-responsivo.js
@@ -2,11 +2,11 @@ let btnMenu = document.querySelector("#menu-principal")
 let menu = document.querySelector("aside");
 
 function acionarMenu() {
-    console.log(menu.style.visibility)
-
     if (menu.style.visibility === "visible") {
         menu.style.visibility = "hidden";
+        btnMenu.innerHTML = '<i class="fa-solid fa-caret-down"></i>'
     } else {
         menu.style.visibility = "visible";
+        btnMenu.innerHTML = '<i class="fa-solid fa-caret-up"></i>'
     }
 }
