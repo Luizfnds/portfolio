e3e844798e53ff0127e491764dd6813fa975d6b1
Luiz Eduardo <luizeduardofnds@gmail.com>
2022-11-09 13:31:43 -0300

    Cencertando bugs - 2

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 const menuLinks = document.querySelectorAll('#ul-nav a[href^="#"]');
+const btnMenu = document.querySelector("#menu-principal")
+const menu = document.querySelector("aside");
 
-function sla(link) {
-
+function adicionarTituloMarcado() {
     document.querySelector(".apresentacao").classList.add("title-marcado")
-
     document.querySelector("main").addEventListener("scroll", (e) => {
 
         let scroll = document.querySelector("main").scrollTop;
         let list = document.querySelectorAll(".title");
+        let altura = document.querySelector("body").clientHeight;
 
-        if (scroll < 400) {
+        if (scroll < (altura/100)*51) {
             list.forEach((e) => { e.classList.remove("title-marcado") });
             document.querySelector(".apresentacao").classList.add("title-marcado")
-        } else if (scroll < 1000) {
+        } else if (scroll < altura/100*151) {
             list.forEach((e) => { e.classList.remove("title-marcado") });
             document.querySelector(".sobre-mim").classList.add("title-marcado")
-        } else if (scroll < 1700) {
+        } else if (scroll < altura/100*251) {
             list.forEach((e) => { e.classList.remove("title-marcado") });
             document.querySelector(".projetos").classList.add("title-marcado")
         } else {
@@ -25,7 +26,23 @@ function sla(link) {
     });
 }
 
-sla();
+function acionarMenu() {
+    if (menu.style.visibility === "visible") {
+        menu.style.visibility = "hidden";
+        btnMenu.innerHTML = '<i class="fa-solid fa-caret-down"></i>'
+    } else {
+        menu.style.visibility = "visible";
+        btnMenu.innerHTML = '<i class="fa-solid fa-caret-up"></i>'
+    }
+}
 
+function medirTela() {
+    window.addEventListener('resize', (e) => {
+        if (document.querySelector("body").clientWidth > 920) {
+            menu.style.visibility = "visible";
+        }
+    });
+}
 
-
+medirTela();
+adicionarTituloMarcado();
